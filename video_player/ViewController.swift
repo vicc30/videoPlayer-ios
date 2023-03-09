526175a62af9e84be8cde977c41f2133223c9f8f
Victor Cruz <victor.cruz.galindo@walmart.com>
2023-03-08 22:49:30 -0600

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Victor Cruz Galindo on 08/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
